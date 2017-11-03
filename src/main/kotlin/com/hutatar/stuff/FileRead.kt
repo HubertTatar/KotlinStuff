16b2ab0cd960f1b636b6881d8b23472a563acb12
Hubert Tatar <hubert.tatar@lingaro.com>
2017-11-03 13:56:09 +0100

    rx

--- src/main/kotlin/com/hutatar/stuff/FileRead.kt
+++ src/main/kotlin/com/hutatar/stuff/FileRead.kt
@@ -1,15 +1,14 @@
 package com.hutatar.stuff
 
-import io.reactivex.Observable
-import io.reactivex.ObservableEmitter
-import io.reactivex.ObservableOnSubscribe
+import io.reactivex.*
+import io.reactivex.disposables.Disposable
 import io.reactivex.functions.Consumer
 import io.reactivex.schedulers.Schedulers
 import java.io.File
 
 fun main(args : Array<String>) {
     val fr = FileRead()
-    fr.secondReader()
+    fr.thirdReader()
     fr.sleep(1000)
 }
 
@@ -41,7 +40,39 @@ class FileRead {
     }
 
     fun thirdReader() {
-//        ObservableEmitter
+        val producer: Observable<String> = Observable.create(ObservableOnSubscribe<String> { emitter ->
+            try {
+                val a: File = File("C:\\Users\\hubert.tatar\\workspace\\server\\apache-tomcat-8.5.9\\logs\\catalina.out")
+                a.useLines {
+                    lines -> lines.forEach { emitter.onNext(it) }
+                }
+                emitter.onComplete()
+            } catch (error: Exception) {
+                emitter.onError(error)
+            }
+        })
+        .subscribeOn(Schedulers.io())
+        .observeOn(Schedulers.io())
+
+        val observer: Observer<String> = object: Observer<String> {
+            override fun onComplete() {
+                println("complete")
+            }
+
+            override fun onError(e: Throwable) {
+                println("error " + e.message)
+            }
+
+            override fun onNext(t: String) {
+                println(t)
+            }
+
+            override fun onSubscribe(d: Disposable) {
+                println("subscribed")
+            }
+        }
+
+        producer.subscribe(observer)
     }
 
     fun sleep(millis: Long) {
